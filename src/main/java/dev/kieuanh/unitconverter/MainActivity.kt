60c87475eb6cce05a0b6435714ddeb88f7543a8b
Kieuanhdev <kieuanh.dev@gmail.com>
2025-06-12 11:41:12 +0700

    part 2 final

--- src/main/java/dev/kieuanh/unitconverter/MainActivity.kt
+++ src/main/java/dev/kieuanh/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package dev.kieuanh.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -25,14 +24,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.kieuanh.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,9 +65,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter(
 
+
+
 ){
 
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("M") }
+    var outputUnit by remember { mutableStateOf("M") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    var iConversionFactor by remember { mutableStateOf(1.0) }
+    var oConversionFactor by remember { mutableStateOf(1.0) }
+
     var context = LocalContext.current
+
+    fun converUnit(){
+        var numberInput = inputValue.toDoubleOrNull() ?: 0.0
+        outputValue = (numberInput*iConversionFactor/oConversionFactor).toString()
+    }
 
     Column (
         modifier = Modifier.fillMaxSize(),
@@ -71,65 +91,117 @@ fun UnitConverter(
         horizontalAlignment = Alignment.CenterHorizontally
 
     ){
-        Text("Unit Converter")
+        Text("Unit Converter", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+                inputValue = it
+                converUnit()
+                },
+
+            label = {Text("Enter value")}
+            )
 //        TextField(value = "kiuenah", onValueChange = {})
 //        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
+            //Box input
             Box{
-                Button(onClick = {}) {
-                    Text("Select")
+                //Button input
+                Button(onClick = {iExpanded = true}) {
+                    Text(inputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {} ){
+                DropdownMenu(expanded = iExpanded, onDismissRequest = {iExpanded = false} ){
                     DropdownMenuItem(
                         text = {Text("cm")},
-                        onClick = {}
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Cm"
+                            iConversionFactor = 0.01
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("dm")},
-                        onClick = {}
+                        text = {Text("Dm")},
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Dm"
+                            iConversionFactor = 0.1
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("m")},
-                        onClick = {}
+                        text = {Text("M")},
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "M"
+                            iConversionFactor = 1.0
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("km")},
-                        onClick = {}
+                        text = {Text("Km")},
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Km"
+                            iConversionFactor = 1000.0
+                            converUnit()
+                        }
                     )
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
+            //output box
             Box{
-                Button(onClick = {}) {
-                    Text("Select")
+                //output button
+                Button(onClick = {oExpanded = true}) {
+                    Text(outputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {} ){
+                DropdownMenu(expanded = oExpanded, onDismissRequest = {oExpanded = false} ){
                     DropdownMenuItem(
-                        text = {Text("cm")},
-                        onClick = {}
+                        text = {Text("Cm")},
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Cm"
+                            oConversionFactor = 0.01
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("dm")},
-                        onClick = {}
+                        text = {Text("Dm")},
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Dm"
+                            oConversionFactor = 0.1
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("m")},
-                        onClick = {}
+                        text = {Text("M")},
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "M"
+                            oConversionFactor = 1.0
+                            converUnit()
+                        }
                     )
                     DropdownMenuItem(
-                        text = {Text("km")},
-                        onClick = {}
+                        text = {Text("Km")},
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Km"
+                            oConversionFactor = 1000.0
+                            converUnit()
+                        }
                     )
                 }
             }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text("Result: ")
+        Text("Result: $outputValue $outputUnit", style = MaterialTheme.typography.headlineMedium)
     }
 }
 
